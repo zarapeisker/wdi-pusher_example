d7e94ae2a54144eea9ad7eba02d891679b96fa03
Sumeet Jain <sumeet@sumeetjain.com>
2013-08-12 08:37:32 -0700

    Setup

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,4 +2,9 @@ class MainController < ApplicationController
   def index
   end
 
+  def send_message
+    @message = params[:message][:text]
+
+    render nothing: true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 PusherExample::Application.routes.draw do
+  post 'send_message' => 'main#send_message', :as => "send_message"
   root :to => "main#index"
 end
